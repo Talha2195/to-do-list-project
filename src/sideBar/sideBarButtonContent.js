932d76b5cc4b07787e9ed5d7d23b8d72e117d546
Talha2195 <143935671+Talha2195@users.noreply.github.com>
2024-05-18 12:31:27 +0300

    Updated CSS

--- src/sideBar/sideBarButtonContent.js
+++ src/sideBar/sideBarButtonContent.js
@@ -2,7 +2,7 @@ import { makeInputDom } from "../miscModules/typeInputDom";
 import { createImage } from "../miscModules/imagesDom";
 function invokeSideBarButton () {
 makeInputDom ('#leftBarContentDiv', 'button', 'Notes', 'newNote');
-createImage('#newNote', 'img', 'plusIcon.png', 'plusIcon', 'plus icon');
+// createImage('#newNote', 'img', 'plusIcon.png', 'plusIcon', 'plus icon');
 makeInputDom ('#leftBarContentDiv', 'button', 'Upcoming', 'upcoming');
 makeInputDom ('#leftBarContentDiv', 'button', 'New Project', 'newProject');
 makeInputDom ('#leftBarContentDiv', 'details', '', 'existingProjects');
